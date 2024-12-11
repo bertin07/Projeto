cd5de2023b47ccdfa8ec60a3fde10e68c29fd50f
lfc26 <lfcc2609@gmail.com>
2024-12-11 10:14:08 -0300

    sons_quase_prontos

--- formatacao_audio_estetica.js
+++ formatacao_audio_estetica.js
@@ -1,2 +1,21 @@
 var audio = document.getElementById("musica_fundo");
 audio.volume = 1;
+
+var btn = document.getElementById("ligar");
+btn.addEventListener("click",function(){
+    if(audio.paused){
+        audio.muted = false;
+        audio.play();
+        icon.src= "img/volume_up_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png"
+    }
+    else{
+        audio.pause();
+    }
+});
+
+var shelly = document.getElementById("shelly_sound")
+var btn = document.getElementById("pin");
+btn.addEventListener("click",function(){
+        shelly.muted = false;
+        shelly.play();
+});
